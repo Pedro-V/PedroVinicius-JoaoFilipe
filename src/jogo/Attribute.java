7c4f441ad80f0a3b2ff55f6589936fde33cb5fb0
Pedro-V <mingas62@hotmail.com>
2022-09-27 01:04:13 -0300

    Attribute agora opera com switches baseados na String valor

--- src/jogo/Attribute.java
+++ src/jogo/Attribute.java
@@ -1,56 +1,59 @@
 package jogo;
 
 public enum Attribute {
-    MONSTER(), CHEFE(), CURA(), QUIZ(),
-    ARMADILHA(), VAZIA();
+    MONSTER("Monstro"), CHEFE("Chefe"), CURA("Cura"), QUIZ("Quiz"),
+    ARMADILHA("Armadilha"), VAZIA("Vazia");
     private String descricao;
+    private String valor;
     // Uma mensagem a ser mostrada caso o atributo ja tenha sido utilizado
     private String mensagemAtributoUtilizado;
     // Um valor associado a ação do atributo
     private int valor_associado;
 
-    Attribute() {
+    Attribute(String valor) {
+        this.valor = valor;
         setDescricaoMensagem();
         inicializaValorAssociado();
     }
 
     private void setDescricaoMensagem(){
-        switch (this) {
-            case MONSTER:
+        switch (valor) {
+            case "Monstro":
                 descricao = "Você encontrou um monstro nessa sala! Ele tem 3 de vida e 1 de dano.\nSó após derrotar ele você pode sair dessa sala.";
                 mensagemAtributoUtilizado = "O monstro dessa sala já está morto.";
                 break;
-            case CHEFE:
+            case "Chefe":
                 descricao = "Você encontrou o chefe! Para obter a safira, você precisa derrotá-lo!\nO chefe tem 8 de vida e 2 de dano! Tome cuidado.";
                 mensagemAtributoUtilizado = "O chefe foi derrotado, parabéns!";
                 break;
-            case CURA:
+            case "Cura":
                 descricao = "Essa sala possui uma aura curadora!\nVocê automaticamente recebeu uma pequena quantia de cura!";
                 mensagemAtributoUtilizado = "";
                 break;
-            case QUIZ:
+            case "Quiz":
                 descricao = "Essa sala possui um erudita! Responda corretamente à charada e ganhe um feitiço de bola de fogo!";
                 mensagemAtributoUtilizado = "O erudita só faz a pergunta uma vez.";
                 break;
-            case ARMADILHA:
+            case "Armadilha":
                 descricao = "Ao entrar nessa sala você ativou uma armadilha automática! Voce sofre uma pequena quantia de dano!";
                 mensagemAtributoUtilizado = "";
                 break;
-            case VAZIA:
+            case "Vazia":
                 descricao = "";
+                mensagemAtributoUtilizado = "";
                 break;
         }
     }
 
     private void inicializaValorAssociado() {
-        switch (this) {
-            case ARMADILHA:
+        switch (valor) {
+            case "Armadilha":
                 valor_associado = -1;
                 break;
-            case CURA:
+            case "Cura":
                 valor_associado = 2;
                 break;
-            case QUIZ:
+            case "Quiz":
                 valor_associado = 3; // valor associado com a bola de fogo
                 break;
             default:
